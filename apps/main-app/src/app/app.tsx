0eb39754bce0d71632b2ed464f6edf407e760e7e
hesam_se <hesam_se@a.com>
2022-01-24 10:36:45 +0330

    add nx workspace

---
+++ apps/main-app/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="main-app" />
+      <div />
+    </>
+  );
+}
+
+export default App;
